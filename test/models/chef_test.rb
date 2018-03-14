cb9ed71581799c85c72482242dd20b227db9bb04
Leslie Kornes <lesliekornes@gmail.com>
2018-03-14 00:51:58 -0400

    Add more chef validation based on failing test

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -20,4 +20,22 @@ class ChefTest < ActiveSupport::TestCase
     assert_not @chef.valid?
   end
 
+  test "chef email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "chef email should not be too long" do
+    @chef.email = "a" * 245 + "@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "email should accept correct format" do
+    valid_emails = %w[user@example.com lesliek@gmail.com andrew@edu.org]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+
 end
