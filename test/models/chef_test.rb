d5cdd14ce50cddf77b9a3d6612d736bac81ff320
Leslie Kornes <lesliekornes@gmail.com>
2018-03-17 22:52:05 -0400

    Add test to ensure emails are lowercase before hitting db

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -53,4 +53,11 @@ class ChefTest < ActiveSupport::TestCase
     assert_not duplicate_chef.valid?
   end
 
+  test "email should be lowercase before hitting db" do
+    mixed_email = "JohN@example.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+
 end
