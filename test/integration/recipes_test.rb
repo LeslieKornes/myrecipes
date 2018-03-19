8ea1b795fc174eb4261b5530fab55862c7399c55
Leslie Kornes <lesliekornes@gmail.com>
2018-03-18 23:38:55 -0400

    Write test to check if index shows list of recipes

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -2,9 +2,23 @@ require 'test_helper'
 
 class RecipesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @chef = Chef.create!(chefname: "Nino", email: "Nino@nkmail.com")
+    @recipe = Recipe.create(name: "Cookies", description: "Gingerbread Ninjas", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "Easter Eggs", description: "Volcano hardboiled eggs")
+    @recipe2.save
+  end
+
   test "should get recipes index" do
     get recipes_url
     assert_response :success
+  end
+
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
   end
 
 end
