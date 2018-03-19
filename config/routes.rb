d5466a3c5bde61bffee862a7491bf9841e2c39fe
Leslie Kornes <lesliekornes@gmail.com>
2018-03-18 23:20:37 -0400

    Add test to find recipes index and add route (manually) to make it pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-root "pages#home"
+  root "pages#home"
 
-get 'pages/home', to: 'pages#home'
+  get 'pages/home', to: 'pages#home'
+
+  get '/recipes', to: 'recipes#index'
 
 end
