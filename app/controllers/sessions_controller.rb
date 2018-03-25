352c1546fde32e370e38a7f6b840398925adb42a
Leslie Kornes <lesliekornes@gmail.com>
2018-03-24 23:52:44 -0400

    Build out create method insessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    chef = Chef.find_by([:session][:email].downcase)
+    if chef && chef.authenticate([:session][:password])
+      #handle it
+    else
+      flash.now[:danger] = "There was a problem logging you in."
+      render 'new'
+    end
   end
 
   def destroy
--- test/integration/chefs_login_test.rb
+++ test/integration/chefs_login_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 class ChefsLoginTest < ActionDispatch::IntegrationTest
 
   test "should reject invalid login" do
-
+    get login_path
+    assert_template 'sessions/new'
   end
 
   test "should accept valid login credentials and log user in" do
-
+    get login_path
+    assert_template 'sessions/new'
   end
 
 end
