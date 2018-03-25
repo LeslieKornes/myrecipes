6ac3aa9355f993e255baee31792af5304be05196
Leslie Kornes <lesliekornes@gmail.com>
2018-03-25 01:57:54 -0400

    Enforce user requirements at controller level (recipes controller).

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [ :show, :edit, :update ]
+  before_action :set_recipe, only: [ :edit, :show, :update ]
+  before_action :require_user, except: [ :index, :show ]
+  before_action :require_same_user, only: [ :destroy, :edit, :update ]
 
   def create
     @recipe = Recipe.new(recipe_params)
@@ -46,8 +48,16 @@ class RecipesController < ApplicationController
   end
 
   private
+
   def recipe_params
     params.require(:recipe).permit(:name, :description)
+  end
+
+  def require_same_user
+    if current_chef != @recipe.chef
+      flash[:danger] = "You can only edit or delete your own recipes!"
+      redirect_to recipes_path
+    end
   end
 
 end
