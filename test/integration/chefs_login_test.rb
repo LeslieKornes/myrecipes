eb383a86b5fbc5da3970e42963946b3bd1e3ce88
Leslie Kornes <lesliekornes@gmail.com>
2018-03-25 00:06:39 -0400

    Fix quite a few silly typos to get tests to pass.

--- test/integration/chefs_login_test.rb
+++ test/integration/chefs_login_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class ChefsLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @chef = Chef.create!(chefname: "Leslie", email: "leslie@lkmail.com", password: "password")
+  end
+
   test "should reject invalid login" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: " ", password: " " } }
-    assert_template 'sessios/new'
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -15,6 +19,11 @@ class ChefsLoginTest < ActionDispatch::IntegrationTest
   test "should accept valid login credentials and log user in" do
     get login_path
     assert_template 'sessions/new'
+    post login_path, params: { session: { email: @chef.email, password: @chef.password } }
+    assert_redirected_to @chef
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_not flash.empty?
   end
 
 end
