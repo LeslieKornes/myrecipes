58a15df44c2feb21bf15ae7c1d0434bbc570ac8b
Leslie Kornes <lesliekornes@gmail.com>
2018-03-24 23:43:57 -0400

    Add integration test for login

---
+++ test/integration/chefs_login_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ChefsLoginTest < ActionDispatch::IntegrationTest
+
+  test "should reject invalid login" do
+
+  end
+
+  test "should accept valid login credentials and log user in" do
+
+  end
+
+end
