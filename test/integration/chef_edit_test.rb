87077aaaca88a34bfe6c5d77c06dc2acc517db9e
Leslie Kornes <lesliekornes@gmail.com>
2018-03-29 05:55:11 -0400

    Add test for admin edit user.

--- test/integration/chef_edit_test.rb
+++ test/integration/chef_edit_test.rb
@@ -5,6 +5,11 @@ class ChefEditTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "Nino", email: "Nino@nkmail.com",
                     password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "Dodjie", email: "Dodjie@dtmail.com",
+                    password: "Dodjie0706", password_confirmation: "Dodjie0706")
+    @admin = Chef.create!(chefname: "Dodjie1", email: "Dodjie1@dtmail.com",
+                    password: "Dodjie0706", password_confirmation: "Dodjie0706",
+                    admin: true)
   end
 
   test "should reject an invalid edit" do
@@ -27,6 +32,22 @@ class ChefEditTest < ActionDispatch::IntegrationTest
     @chef.reload
     assert_match "Nino1", @chef.chefname
     assert_match "nino1@nkmail.com", @chef.email
+  end
+
+  test "should accept edit attempt by adnin user" do
+    sign_in_as(@admin, "Dodjie0706")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "Nino2", email: "nino2@nkmail.com" } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Nino2", @chef.chefname
+    assert_match "nino2@nkmail.com", @chef.email
+  end
+
+  test "should redirect edit attempt by another non-admin user" do
+
   end
 
 end
