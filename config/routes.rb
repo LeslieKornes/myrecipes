4605577d7c3b19d2ae70dabb39bd5c3c92315d17
Leslie Kornes <lesliekornes@gmail.com>
2018-03-31 08:08:16 -0400

    Update navbar and clean up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  get 'pages/home', to: 'pages#home'
-
-  resources :recipes
-
-  resources :ingredients
-
-  get '/signup', to: 'chefs#new'
-  resources :chefs, except: [:new]
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get 'pages/home', to: 'pages#home'
+  get '/signup', to: 'chefs#new'
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  resources :chefs, except: [:new]
+  resources :ingredients, except: [:destroy]
+  resources :recipes
 
 end
