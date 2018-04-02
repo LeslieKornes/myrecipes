4d1c4eed639a52f81f0578e892804076ed09611f
Leslie Kornes <lesliekornes@gmail.com>
2018-04-02 00:27:07 -0400

    Complete new/create and edit/update actions in ingredient controller.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,7 +3,13 @@ class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [ :edit, :update, :show ]
 
   def create
-
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient was successfully created!"
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,10 +29,19 @@ class IngredientsController < ApplicationController
   end
 
   def update
-
+    if @ingredient.update
+      flash[:success] = "Ingredient was successfully upadated!"
+      redirect_to @ingredient
+    else
+      render 'edit'
+    end
   end
 
   private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 
   def set_ingredient
     @ingredient = Ingredient.find(params[:id])
